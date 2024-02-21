b265f851bcc65216b14468d8d64f947fa5c6bb48
Filip Gajic <Filip.Gajic@niceactimize.com>
2024-02-21 16:03:26 +0100

    wip

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,74 +1,69 @@
-import React from 'react';
-import { MDXRemote } from 'next-mdx-remote';
+import { CH } from "@code-hike/mdx/components";
+import React from "react";
+import Divider from "@/components/Divider";
+import { MDXRemote } from "next-mdx-remote";
 import { serialize } from 'next-mdx-remote/serialize';
-import fs from 'fs/promises'; // Import fs to read MDX file
-import path from 'path'; // Import path to resolve file paths
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import BlogImage from "@/components/BlogImage";
-import { CH } from '@code-hike/mdx/components';
 
-// Define component types
-type ComponentProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-
-// Define components
-const components: { [key: string]: React.FC<ComponentProps> } = {
-  CH,
-  h1: (props) => (
-      <h1 className={"post-h1"} {...props} />
-  ),
-  h2: (props) => (
-      <h2 className={"post-h2 on-surface-text secondary-text mt-8"} {...props} />
-  ),
-  h3: (props) => (
-      <h3 className={"post-h3 on-surface-text secondary-text mt-8"} {...props} />
-  ),
-  p: (props) => (
-      <p className={"post-p on-surface-text mt-2"} {...props} />
-  ),
-  a: (props) => (
-      <a className={"post-p on-surface-text mt-2 primary-text hover:underline"} {...props} />
-  ),
-  ol: (props) => (
-      <ol className={"post-p on-surface-text mt-2 list-decimal list-inside"} {...props}></ol>
-  ),
-  ul: (props) => (
-      <ul className={"post-p on-surface-text mt-2 list-disc list-inside"} {...props}></ul>
-  ),
-  li: (props) => (
-      <li className={"pt-2"} {...props}></li>
-  ),
-  BlogImage: (props: { src: string, sub: string, alt: string, width: number, height: number }) => (
-      <BlogImage src={props.src} sub={props.sub} alt={props.alt} width={props.width} height={props.height}/>
-  ),
-  code: (props) => (
-      <code className={"code"} {...props} />
-  )
+interface AboutPageProps {
+  content: any; // Adjust the type as per your MDX content structure
 }
 
-// Function to read MDX file
-const readMDXFile = async (filePath: string) => {
-  const fullPath = path.resolve(process.cwd(), filePath);
-  const mdxContent = await fs.readFile(fullPath, 'utf8');
-  const mdxSource = await serialize(mdxContent);
-  return mdxSource;
-};
+const components = {
+    CH,
+    h1: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) => (
+        <h1 className={"post-h1"} {...props} />
+    ),
+    h2: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) => (
+        <h2 className={"post-h2 on-surface-text secondary-text mt-8"} {...props} />
+    ),
+    h3: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) => (
+        <h3 className={"post-h3 on-surface-text secondary-text mt-8"} {...props} />
+    ),
+    p: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>) => (
+        <p className={"post-p on-surface-text mt-2"} {...props} />
+    ),
+    a: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => (
+        <a className={"post-p on-surface-text mt-2 primary-text hover:underline"} {...props} />
+    ),
+    ol: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLOListElement>, HTMLOListElement>) => (
+        <ol className={"post-p on-surface-text mt-2 list-decimal list-inside"} {...props}></ol>
+    ),
+    ul: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement>) => (
+        <ul className={"post-p on-surface-text mt-2 list-disc list-inside"} {...props}></ul>
+    ),
+    li: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLLIElement>, HTMLLIElement>) => (
+        <li className={"pt-2"} {...props}></li>
+    ),
+    code: (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>) => (
+        <code className={"code"} {...props} />
+    )
+}
 
-// About component
-const About: React.FC<{ mdxSource: any }> = ({ mdxSource }) => {
+const AboutPage: React.FC<AboutPageProps> = ({ content }) => {
   return (
-    <div className="container mx-auto px-4">
-      <MDXRemote  {...mdxSource} components={components}/>
-    </div>
+      <div className={"mt-4 flex w-full align-middle justify-center"}>
+          <div className={"min-w-full"}>
+              <MDXRemote {...content} components={components}/>
+          </div>
+      </div>
   );
 };
 
-// Fetch MDX content and pass it to About component
-export const getStaticProps = async () => {
-  const mdxSource = await readMDXFile('./src/pages/about_me.mdx');
-  return {
-    props: {
-      mdxSource,
-    },
-  };
-};
+export async function getStaticProps() {
+  const aboutMeFilePath = path.join(process.cwd(),'./src/pages/about_me.mdx'); // Adjust the path as per your file structure
+  const fileContents = fs.readFileSync(aboutMeFilePath, 'utf8');
+  const { content, data } = matter(fileContents);
+  const mdxSource = await serialize(content);
 
-export default About;
+  return {
+      props: {
+          content: mdxSource,
+      },
+  };
+}
+
+export default AboutPage;
