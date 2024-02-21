8e396cb6a5a370731dded785778f87b96ba08325
Filip Gajic <Filip.Gajic@niceactimize.com>
2024-02-21 17:01:25 +0100

    added working hamburger

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -25,34 +25,25 @@ export default function Navbar() {
           <div className={"flex"}>
             <NavbarHome />
             <button className={"ml-auto rounded"} onClick={toggleMobileMenu}>
-              {
-                mobileMenuOpen &&
-                <svg
-                  className={"fill-[--md-sys-color-on-surface]"}
-                  xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960"
-                  width="32">
-                  {/* SVG path */}
-                </svg>
-              }
-              {
-                !mobileMenuOpen &&
-                <svg
-                  className={"fill-[--md-sys-color-on-surface]"}
-                  xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960"
-                  width="32">
-                  {/* SVG path */}
-                </svg>
-              }
+              <svg
+                className={"fill-[--md-sys-color-on-surface]"}
+                xmlns="http://www.w3.org/2000/svg"
+                height="32"
+                viewBox="0 0 24 24"
+                width="32"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M4 18h16c.552 0 1-.448 1-1s-.448-1-1-1H4c-.552 0-1 .448-1 1s.448 1 1 1zm0-5h16c.552 0 1-.448 1-1s-.448-1-1-1H4c-.552 0-1 .448-1 1s.448 1 1 1zM3 7c0 .552.448 1 1 1h16c.552 0 1-.448 1-1s-.448-1-1-1H4c-.552 0-1 .448-1 1z" />
+              </svg>
             </button>
           </div>
-          {
-            mobileMenuOpen &&
+          {mobileMenuOpen && (
             <div className={"flex"}>
               <div className={"flex-col space-y-4 mt-4 ml-10"}>
                 <NavbarContent isMobile={true} onClick={toggleMobileMenu} />
               </div>
             </div>
-          }
+          )}
         </div>
 
         {/* Desktop */}
@@ -64,14 +55,14 @@ export default function Navbar() {
         <Divider className={"on-surface-text"} />
       </div>
     </>
-  )
+  );
 }
 
 type LinkClickedCallback = () => void;
 type NavbarContentProps = {
-  isMobile: boolean,
-  onClick: LinkClickedCallback
-}
+  isMobile: boolean;
+  onClick: LinkClickedCallback;
+};
 
 function NavbarContent({ isMobile, onClick }: NavbarContentProps) {
   let textClass = "on-surface-text ";
@@ -84,7 +75,7 @@ function NavbarContent({ isMobile, onClick }: NavbarContentProps) {
       <NavbarLink href={"/about"} onClick={onClick}><p className={textClass}>About</p></NavbarLink>
       <div className={"flex items-center ml-auto"}>
         <NavbarLink href={AuthorInfo.linkedInUrl} openNewWindow>
-          <svg className={"fill-[--md-sys-color-on-surface]"} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <svg className={"fill-[--md-sys-color-on-surface]"} xmlns="http://www.w3.org/2000/svg" width="24" height="24">
           <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
           </svg>
         </NavbarLink>
