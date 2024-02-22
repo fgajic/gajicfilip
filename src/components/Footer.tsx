1e47c9bcad98a505d7a0ba6b6e6e51d66e25ce05
Filip Gajic <Filip.Gajic@niceactimize.com>
2024-02-22 17:12:54 +0100

    adding cookies support

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,22 @@
 import Divider from "@/components/Divider";
-import React from "react";
+import React, { useEffect } from "react";
 import { maxWidthClass } from "@/utils/styling";
 import { AuthorInfo } from "@/domain/author";
+import CookieConsent from "react-cookie-consent";
 
 export default function Footer() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.charset = 'UTF-8';
+        script.src = '//cdn.cookie-script.com/s/48fe9c85535dd2497cff228183eed638.js';
+        document.head.appendChild(script);
+
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
+
     return (
         <footer className={`mt-auto ${maxWidthClass}`}>
             <div className="mt-12">
@@ -11,10 +24,16 @@ export default function Footer() {
                 <div className="flex min-h-full text-center h-14">
                     <div className="py-2 px-5 min-w-full flex flex-row items-center">
                         <p className="outline-text">© 2024 {AuthorInfo.fullName}. All rights reserved.</p>
-                        {/* <a id="csconsentlink" className="outline-text hover:underline cursor-pointer ml-auto">Cookies</a> */}
+                        <a id="csconsentlink" className="outline-text hover:underline cursor-pointer ml-auto">Cookies</a>
                     </div>
                 </div>
             </div>
+            <CookieConsent
+                buttonText="I understand"
+                buttonStyle={{ background: "#4e503b", color: "#fff", fontSize: "13px" }}
+            >
+                This website uses cookies to enhance the user experience.
+            </CookieConsent>
         </footer>
     );
 }
